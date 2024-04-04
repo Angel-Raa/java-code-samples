1d341ffaf73fe1317db21e1ce1dc973e735d8ad4
Angel Aguero <angelagueror23@gmail.com>
2024-04-03 20:10:51 -0400

    first commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+k
+;src/main/java/com/github/angel/models/stream/ApiStream.java,c/3/c38e20e08c0aa25f65ceb63d1a2eee47fbc2e8b3
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
---
+++ src/main/java/com/github/angel/models/stream/ApiStream.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.github.angel.models.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+
+/**
+ *
+ * @author aguero
+ */
+public class ApiStream {
+    public static void main(String[] args) {
+        // Stream vacía
+        Stream<String > emptyStream = Stream.empty();
+
+    }
+}
