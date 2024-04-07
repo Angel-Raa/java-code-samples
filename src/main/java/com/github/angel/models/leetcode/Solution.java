4359560e56fb5340ddd986561211e5c345e33eaa
Angel Aguero <angelagueror23@gmail.com>
2024-04-07 11:28:51 -0400

    169. Majority Element

--- src/main/java/com/github/angel/models/leetcode/Solution.java
+++ src/main/java/com/github/angel/models/leetcode/Solution.java
@@ -5,13 +5,8 @@
 
 package com.github.angel.models.leetcode;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 /**
  *
@@ -19,7 +14,8 @@ import java.util.stream.Stream;
  */
 public class Solution {
     public static void main(String[] args) {
-        System.out.println(isPalindrome(313));
+        int[] numbers = new int[] { 3, 3, 4 };
+        System.out.println(majorityElement(numbers));
 
     }
 
@@ -66,12 +62,55 @@ public class Solution {
     }
 
     static public int removeDuplicates(int[] nums) {
-        int count = 0;
-       
-         
-        
+        if (nums.length == 0) {
+            return 0; // Si el arreglo está vacío, no hay elementos únicos
+        }
 
-        return count;
+        int uniqueElements = 1; // Contador para los elementos únicos, inicializado en 1 para contar el primer
+                                // elemento único
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[uniqueElements] = nums[i]; // Mueve el elemento único al lugar correspondiente en el arreglo
+                uniqueElements++; // Incrementa el contador de elementos únicos
+            }
+        }
+
+        return uniqueElements;
     }
+
+    static public int majorityElement(int[] nums) {
+        int candidate = 0; // Inicializamos el candidato a elemento mayoritario
+        int count = 0; // Contador para el candidato
+    
+        for (int num : nums) { // Recorremos el arreglo
+            if (count == 0) { // Si el contador es 0, asignamos el número actual como candidato
+                candidate = num;
+            }
+    
+            if (num == candidate) { // Si el número actual es igual al candidato, aumentamos el contador
+                count++;
+            } else { // Si el número actual es diferente al candidato, reducimos el contador
+                count--;
+            }
+        }
+    
+        // En este punto, candidate contiene el posible elemento mayoritario
+        // Verificamos si realmente es el elemento mayoritario contando su frecuencia
+        int frequency = 0;
+        for (int num : nums) {
+            if (num == candidate) {
+                frequency++;
+            }
+        }
+    
+        // Si la frecuencia del candidato es mayor que la mitad del tamaño del arreglo, entonces es el elemento mayoritario
+        if (frequency > nums.length / 2) {
+            return candidate;
+        } else {
+            return -1; // No hay elemento mayoritario
+        }
+    }
+    
 
 }
